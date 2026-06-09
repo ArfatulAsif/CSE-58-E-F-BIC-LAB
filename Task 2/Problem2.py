c4214251b88fe901e564314d03955171119c50b4
Marjia275 <140470009+Marjia275@users.noreply.github.com>
2026-06-09 16:01:56 +0600

    Update Problem2.py

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,20 @@
+DNA_S=input()
+k=int(input())
+c={}
+for i in range (len(DNA_S)-k+1):
 
+    kmer=DNA_S[i:i+k]
+
+    if kmer in c:
+        c[kmer]=c[kmer]+1
+    else:
+        c[kmer]=1
+l=0
+
+for kmer in c:
+    if c[kmer]>l:
+        l=c[kmer]
+
+for kmer in c:
+    if c[kmer]==l:
+        print(kmer)
